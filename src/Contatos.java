cfd2f9d8b10e99e592425f0aed65f5e1951b0cdd
Marcus Vinicius <marcusmendes746@gmail.com>
2025-04-08 20:04:48 -0300

    Corrigindo erros nos metodos.

--- src/Contatos.java
+++ src/Contatos.java
@@ -42,14 +42,22 @@ public class Contatos {
         this.email = email;
     }
 
+    @Override
     public String toString() {
-        return " \nNome: " + nome + "\nTelefone: " + telefone + "\nEmail: " + email;
+        return "Contatos [nome=" + nome + ", telefone=" + telefone + ", email=" + email + "]";
     }
 
     public void armazenarContato(String nome, String telefone, String email) {
+        for (Contatos contato : contatos) {
+            if (contato.getNome().equals(nome)) {
+                System.out.println("Já existe um contato com esse nome.");
+                return;
+            }
+        }
         Contatos contato = new Contatos(nome, telefone, email);
         contatos.add(contato);
         System.out.println("Contato cadastrado com sucesso!");
+        System.out.println(contatos.size());
 
     }
 
@@ -61,17 +69,25 @@ public class Contatos {
             System.out.println("Lista de contatos:");
             for (int i = 0; i < contatos.size(); i++) {
 
-                System.out.println((i + 1) + " " + contatos);
-                i++;
+                Contatos contato = contatos.get(i);
+                int indice = i + 1;
+                System.out.println(indice + "- Nome:" + contato.getNome() + " Telefone:" + contato.getTelefone()
+                        + " Email:" + contato.getEmail());
+
             }
         }
     }
 
     public void editarContato() {
+
+        if (contatos.isEmpty()) {
+            System.out.println("Não há contatos cadastrados.");
+            return;
+        }
         System.out.println("Lista de contatos:");
         for (int i = 0; i < contatos.size(); i++) {
-            System.out.println((i + 1) + " " + contatos);
-            i++;
+            System.out.println((i + 1) + "- " + contatos.get(i));
+            
         }
         System.out.println("Digite o número do contato que deseja editar");
         int numero = scanner.nextInt();
@@ -89,10 +105,15 @@ public class Contatos {
     }
 
     public void removerContato() {
+        if (contatos.isEmpty()) {
+            System.out.println("Não há contatos cadastrados.");
+            return;
+        }
+
         System.out.println("Lista de contatos:");
         for (int i = 0; i < contatos.size(); i++) {
-            System.out.println((i + 1) + " " + contatos);
-            i++;
+            System.out.println((i + 1) + "- " + contatos.get(i));
+            
         }
         System.out.println("Digite o numero do contato que deseja remover");
         int numero = scanner.nextInt();
